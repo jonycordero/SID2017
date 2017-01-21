d6a387eb5e129c1159cf2d50723e9705e31093b2
Pabel <Pabel@Pabel-PC.mshome.net>
2017-01-20 19:59:29 -0600

    20012017

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel para inutiles
                 </div>
 
                 <div class="links">
